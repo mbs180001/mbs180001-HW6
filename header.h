15719d0d5c322fb85a88728bf79efed62e498beb
mbs180001 <mbs180001@cslinux1.utdallas.edu>
2020-11-22 22:29:48 -0600

    Added a secondary source file and a header file

---
+++ header.h
@@ -0,0 +1,23 @@
+/* Filename            header.h
+ * Date                November 22, 2020
+ * Author              Michael Sobieski
+ * Email               mbs180001@utdallas.edu
+ * Version             1.0
+ * Copyright           2020, All Rights Reserved
+ * Description         A headerfile for including libraries and prototyping functions
+ */
+
+#include <iostream>
+#include "cdk.h"
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <stdint.h>
+#include <inttypes.h>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void printSuccess();
---
+++ printSuccess.cc
@@ -0,0 +1,16 @@
+/* Filename:   printSuccess.cc
+ * Date:       22 November 2020
+ * Author:     Michael Sobieski
+ * Email:      mbs180001@utdallas.edu
+ * Version:    1.0
+ * Copyright:  2020, All rights reserved
+ * Description:   A program to print a success message at the end of main.cc, as I have a flight in the morning and do not have 
+ *                time to split up my main function into more complex functions. Sorry.
+ */
+
+#include "header.h"
+
+void printSuccess()
+{
+  cout << "Successfully completely program." << endl;
+}
